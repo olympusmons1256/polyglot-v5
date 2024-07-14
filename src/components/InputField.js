1ad9e6c39df5ead220125096c4290fa207ec94ad
olympusmons1256 <142126541+olympusmons1256@users.noreply.github.com>
2024-07-14 16:57:39 -0700

    react app setup

---
+++ src/components/InputField.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Textfield } from '@react-spectrum/textfield';
+
+const InputField = () => {
+  return <Textfield placeholder="Enter text here" />;
+};
+
+export default InputField;
