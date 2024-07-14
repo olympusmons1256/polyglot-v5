be9c1bb0c79d9087fcbb356adbb073c98a2946ca
olympusmons1256 <142126541+olympusmons1256@users.noreply.github.com>
2024-07-14 16:57:03 -0700

    `Commit: Create basic structure with side menu, input field, and canvas area`

---
+++ src/components/SideMenu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Menu, MenuItem } from '@react-spectrum/menu';
+
+const SideMenu = () => {
+  return (
+    <Menu>
+      <MenuItem>Item 1</MenuItem>
+      <MenuItem>Item 2</MenuItem>
+      <MenuItem>Item 3</MenuItem>
+    </Menu>
+  );
+};
+
+export default SideMenu;
